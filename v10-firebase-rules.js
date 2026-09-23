d7c3db5e32945f46bfbaf7fe85dc8aca0190da99
Syla145 <jhwindach@gmx.de>
2026-09-23 13:45:03 +0200

    Löschen

--- v10-firebase-rules.js
+++
@@ -1,9 +0,0 @@
-const fs=require('fs'),path=require('path');const root=path.resolve(__dirname,'..');let pass=0,fail=0;const ok=(c,m)=>{if(c)pass++;else{fail++;console.error('FAIL',m)}};
-const rules=JSON.parse(fs.readFileSync(path.join(root,'firebase-database.rules.json'),'utf8')).rules.rooms['$code'];
-ok(Boolean(rules.buzzerClaims),'buzzerClaims rules exist');
-ok(Boolean(rules.buzzerBlocked),'buzzerBlocked rules exist');
-ok(String(rules.buzzerClaims['$qid']['.write']).includes('contenderId'),'buzzer claim validates contender identity');
-ok(String(rules.buzzerClaims['$qid']['.write']).includes('questionOpen'),'buzzer claim requires open question');
-ok(String(rules.buzzerClaims['$qid']['.write']).includes('buzzerBlocked'),'blocked player cannot reclaim buzzer');
-ok(String(rules.answers['$uid']['$qid']['.write']).includes('buzzerClaims'),'buzzer answer write requires winning claim');
-console.log(`PASS ${pass} / FAIL ${fail}`);process.exit(fail?1:0);
